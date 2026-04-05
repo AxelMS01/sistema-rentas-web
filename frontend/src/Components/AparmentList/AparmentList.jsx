19e225901bd202a9e3a42e1cb60bfa2230608e98
Kevin Torres <kevintu236@gmail.com>
2026-04-05 13:46:09 -0600

    Cambio de visualización del PDF.

--- frontend/src/Components/AparmentList/AparmentList.jsx
+++ frontend/src/Components/AparmentList/AparmentList.jsx
@@ -212,7 +212,7 @@ const Viviendas = () => {
         </div>
 
         <PDFViewer width={500} height={800}>
-          <DocumentoContrato />
+          <DocumentoPagare />
         </PDFViewer>
 
         {/* STATUS INDICATORS */}
