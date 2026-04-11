5591095f4bb885673795c9b8e1a4796075aba781
Kevin Torres <kevintu236@gmail.com>
2026-04-10 19:34:34 -0600

    Creación de nuevos arrendadores.
    Se terminó de configurar la funcionalidad para que nuevos arrendadores puedan registrarse y entrar a su espacio individual.

--- frontend/src/Components/LoginForm/LoginForm.jsx
+++ frontend/src/Components/LoginForm/LoginForm.jsx
@@ -4,14 +4,16 @@ import { FaUser, FaEnvelope } from "react-icons/fa6";
 import { FaLock } from "react-icons/fa6";
 import { useNavigate } from 'react-router-dom';
 import useUser from '../../Stores/user-store';
+import { supabase } from '../../Config/supabase-client';
 
 const LoginForm = () => {
 
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassowrd, setUserPassword] = useState("");
   const [action, setAction] = useState('');
   const [faqOpenIndex, setFaqOpenIndex] = useState(0);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  
   const navigate = useNavigate();
   const updateUserId = useUser((state) => state.updateLoggedUser);
 
@@ -29,6 +31,12 @@ const LoginForm = () => {
 
   const handleLogin = async (e) => {
     e.preventDefault();
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: userEmail,
+      password: userPassowrd
+    });
+
     try {
       /*
       const response = await fetch(api("/login"), {
@@ -75,18 +83,18 @@ const LoginForm = () => {
           <form onSubmit={handleLogin}>
             <h1>Administración de Rentas</h1>
             <div className="input-box">
-              <input type="text" placeholder='Username' onChange={(e) => setName(e.target.value)} required /> <FaUser className='icon' />
+              <input type="text" placeholder='Correo electrónico' onChange={(e) => setUserEmail(e.target.value)} required /> <FaUser className='icon' />
             </div>
 
             <div className="input-box">
-              <input type="password" placeholder='Password' onChange={(e) => setPassword(e.target.value)} required /> <FaLock className='icon' />
+              <input type="password" placeholder='Contraseña' onChange={(e) => setUserPassword(e.target.value)} required /> <FaLock className='icon' />
             </div>
             <div className="remember-forgot">
-              <label><input type='checkbox' />Recuerdame</label>
+              <label><input type='checkbox' />Recuérdame</label>
               <a href='#'> </a>
             </div>
 
-            <button type="submit" className="btn btn-dark w-100">Login</button>
+            <button type="submit" className="btn btn-dark w-100">Iniciar sesión</button>
 
             <div className="register-link">
               <p> <a href='#' onClick={registerLink}> </a>
