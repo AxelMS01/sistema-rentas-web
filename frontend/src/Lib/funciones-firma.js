6041cd1de013cbba32b7d09f833af1ce5af85694
Kevin Torres <kevintu236@gmail.com>
2026-04-14 21:08:18 -0600

    Removiendo archivos anteriores
    - Se removió la carpeta anterior debido a que ahora se está usando  el ecosistema de Vite con estos mismos.

--- frontend/src/Lib/funciones-firma.js
+++
@@ -1,7 +0,0 @@
-// GRUPO DE FUNCIONES QUE MANEJAN TODO LO RELACIONADO CON LA FIRMA DE LOS USUARIOS.
-
-function guardarNuevaFirma(imagen) {
-
-};
-
-export { guardarNuevaFirma };
--- frontend/src/Utils/mensaje-exito.js
+++
@@ -1,7 +0,0 @@
-import toast from "react-hot-toast";
-
-function mensajeExito(mensaje) {
-    toast.success(mensaje, { duration: 2000 });
-};
-
-export default mensajeExito;
