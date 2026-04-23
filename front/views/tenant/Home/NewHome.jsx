42a428b894f3051b557402c4d76ef9da9592449a
Kevin Torres <kevintu236@gmail.com>
2026-04-22 18:28:33 -0600

    Ajuste rápido al iniciar sesión sin contrato.

--- front/views/tenant/Home/NewHome.jsx
+++ front/views/tenant/Home/NewHome.jsx
@@ -46,6 +46,8 @@ const Home = () => {
 
                 if (contractError) throw contractError;
 
+                console.log(contractData.length);
+
                 if (contractData.length === 0) {
                     navigate("/", {
                         state: {
@@ -70,54 +72,58 @@ const Home = () => {
 
     return (
         <>
-            <ContractDetailsModal
-                isModalOpen={contractDetailModal}
-                onCloseModal={() => setContractDetail(false)}
-                ownerId={ownerId}
-                tenantId={loggedUserId}
-            />
-
             {!isLoading && (
-                <div className="w-full min-h-screen flex flex-col gap-4! lg:px-20! sm:px-16! px-8! py-10">
-                    <div className="flex w-full md:flex-row flex-col justify-between md:items-center items-start gap-6">
-                        <div className="header flex flex-col gap-2">
-                            <h1 className="text-start font-light fw-semibold tracking-tight">¡Bienvenido, {tenantInfo.name}!</h1>
-                            <p className="text-base font-normal text-slate-500 text-start">¿Qué deseas hacer hoy?</p>
+
+                <>
+                    <ContractDetailsModal
+                        isModalOpen={contractDetailModal}
+                        onCloseModal={() => setContractDetail(false)}
+                        ownerId={ownerId}
+                        tenantId={loggedUserId}
+
+                    />
+
+                    <div className="w-full min-h-screen flex flex-col gap-4! lg:px-20! sm:px-16! px-8! py-10">
+                        <div className="flex w-full md:flex-row flex-col justify-between md:items-center items-start gap-6">
+                            <div className="header flex flex-col gap-2">
+                                <h1 className="text-start font-light fw-semibold tracking-tight">¡Bienvenido, {tenantInfo.name}!</h1>
+                                <p className="text-base font-normal text-slate-500 text-start">¿Qué deseas hacer hoy?</p>
+                            </div>
+
+                            <div className="flex flex-row gap-2 items-center justify-center px-3 py-2 bg-sky-600 border border-sky-700 text-white rounded-md self-start text-sm! font-medium">
+                                <Sun strokeWidth={2} size={18} />
+                                Hoy es {format(todayDate, "PPP", { locale: es })}
+                            </div>
                         </div>
 
-                        <div className="flex flex-row gap-2 items-center justify-center px-3 py-2 bg-sky-600 border border-sky-700 text-white rounded-md self-start text-sm! font-medium">
-                            <Sun strokeWidth={2} size={18} />
-                            Hoy es {format(todayDate, "PPP", { locale: es })}
+                        <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
+                            <MenuCard action="quick">
+                                <div className="w-full flex! cursor-pointer py-2 px-3 flex-row! gap-2 items-center justify-start border border-slate-200 rounded-md hover:bg-sky-100 hover:border-sky-500!">
+                                    <Banknote size={20} />
+                                    <p className="font-medium! text-sm! text-slate-700">Realizar un pago</p>
+                                </div>
+
+                                <div onClick={() => setContractDetail(true)} className="w-full flex! cursor-pointer py-2 px-3 flex-row! gap-2 items-center justify-start border border-slate-200 rounded-md hover:bg-sky-100 hover:border-sky-500!">
+                                    <ScrollText size={20} />
+                                    <p className="font-medium! text-sm! text-slate-700">Ver detalles de mi contrato</p>
+                                </div>
+
+                                <div onClick={() => navigate("/mis-incidencias")} className="w-full flex! cursor-pointer py-2 px-3 flex-row! gap-2 items-center justify-start border border-slate-200 rounded-md hover:bg-sky-100 hover:border-sky-500!">
+                                    <TriangleAlert size={20} />
+                                    <p className="font-medium! text-sm! text-slate-700">Mis incidencias</p>
+                                </div>
+                            </MenuCard>
+
+                            <MenuCard action="info">
+                                <HomeInfoCard />
+                            </MenuCard>
+
+                            <MenuCard
+                                action="receipts"
+                            />
                         </div>
                     </div>
-
-                    <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
-                        <MenuCard action="quick">
-                            <div className="w-full flex! cursor-pointer py-2 px-3 flex-row! gap-2 items-center justify-start border border-slate-200 rounded-md hover:bg-sky-100 hover:border-sky-500!">
-                                <Banknote size={20} />
-                                <p className="font-medium! text-sm! text-slate-700">Realizar un pago</p>
-                            </div>
-
-                            <div onClick={() => setContractDetail(true)} className="w-full flex! cursor-pointer py-2 px-3 flex-row! gap-2 items-center justify-start border border-slate-200 rounded-md hover:bg-sky-100 hover:border-sky-500!">
-                                <ScrollText size={20} />
-                                <p className="font-medium! text-sm! text-slate-700">Ver detalles de mi contrato</p>
-                            </div>
-
-                            <div onClick={() => navigate("/mis-incidencias")} className="w-full flex! cursor-pointer py-2 px-3 flex-row! gap-2 items-center justify-start border border-slate-200 rounded-md hover:bg-sky-100 hover:border-sky-500!">
-                                <TriangleAlert size={20} />
-                                <p className="font-medium! text-sm! text-slate-700">Mis incidencias</p>
-                            </div>
-                        </MenuCard>
-
-                        <MenuCard action="info">
-                            <HomeInfoCard />
-                        </MenuCard>
-
-                        <MenuCard
-                            action="receipts"
-                        />
-                    </div>
-                </div>
+                </>
             )}
         </>
     );
